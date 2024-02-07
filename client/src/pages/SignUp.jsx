153d2143ac30d596a2789ba882833bfb994b6a70
Sagar Wagdare <sagarwagdare2001@gmail.com>
2024-02-07 22:13:32 +0530

    complete sign up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,34 @@
-import React from "react";
-import {Link} from "react-router-dom"
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
+import axios from "axios";
 const SignUp = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios
+      .post("/api/auth/signup", formData)
+      .then((res) => {
+        setLoading(false);
+        navigate("/sign-in");
+      })
+      .catch((err) => {
+        setError(err?.response?.data?.message);
+        setLoading(false);
+      });
+  };
+  console.log(formData);
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-5 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -11,15 +38,16 @@ const SignUp = () => {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        <form className="space-y-6" onSubmit={handleSubmit} method="POST">
           <div>
             <div className="mt-2">
               <input
-                id="email"
-                name="email"
-                type="email"
+                id="username"
+                name="username"
+                type="text"
                 placeholder="Username"
                 autoComplete="name"
+                onChange={handleChange}
                 required
                 className="outline-none p-2 block w-full rounded-md  py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -33,6 +61,7 @@ const SignUp = () => {
                 type="email"
                 autoComplete="email"
                 placeholder="Email"
+                onChange={handleChange}
                 required
                 className="p-2 block w-full rounded-md  py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -48,6 +77,7 @@ const SignUp = () => {
                 type="password"
                 autoComplete="current-password"
                 placeholder="Password"
+                onChange={handleChange}
                 required
                 className="p-2 block w-full rounded-md  py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -57,28 +87,29 @@ const SignUp = () => {
               Forgot password?
             </a>
           </div> */}
-            <div className="text-sm pt-1 flex gap-1">
-              <p>Already have an account?</p>
-              <Link to="/sign-in" className="text-blue-800 font-semibold">Sign in</Link>
-            </div>
           </div>
-
+          <span className="text-red-600 p-2">{error}</span>
           <div>
             <button
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Sign up
+              {loading ? "Loading..." : "Sign up"}
             </button>
+          </div>
+          <div className="text-sm pt-1 flex gap-1">
+            <p>Already have an account?</p>
+            <Link to="/sign-in" className="text-blue-800 font-semibold">
+              Sign in
+            </Link>
           </div>
           <span className="flex justify-center  items-center">or</span>
           <div className="">
-            
             <button
               type="submit"
               className="flex w-full items-center gap-1 justify-center rounded-md bg-white text-black px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm hover:bg-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-               <FcGoogle/>
+              <FcGoogle />
               Log in with Google
             </button>
           </div>
