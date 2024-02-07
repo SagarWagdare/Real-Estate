28ab8d81d58cfe6b2b78f6c788a2dc64c8e7991e
Sagar Wagdare <sagarwagdare2001@gmail.com>
2024-02-07 22:48:03 +0530

    create api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 export const signup= async (req,res,next)=>{
   const {username,email,password} = req.body;
   const hashPassword = bcrypt.hashSync(password,10)
@@ -12,4 +14,29 @@ export const signup= async (req,res,next)=>{
     next(error);
     
   }
+  }
+
+  export const signin = async (req,res,next)=>{
+    const {email ,password} = req.body;
+
+    try {
+      const validuser = await User.findOne({email})
+      if(!validuser){
+        return next(errorHandler(404,"User not found!"))
+      }
+      const validPassword = bcrypt.compareSync(password,validuser.password)
+      if(!validPassword){
+        return next(errorHandler(401,"Wrong credentials!"))
+      }
+      const token = jwt.sign({
+        id:validuser._id
+      },process.env.JWT_SECRET)
+
+      const {password:pass, ...rest} = validuser._doc
+      res.cookie('access_token',token,{
+        httpOnly:true,
+      }).status(200).json(rest)
+    } catch (error) {
+      next(error)
+    }
   }
