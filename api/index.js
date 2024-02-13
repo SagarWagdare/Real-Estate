5e08ba0f72ed5b6c4d86b4157cdcf6f929b4b2ab
Sagar Wagdare <sagarwagdare2001@gmail.com>
2024-02-13 22:55:18 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import path from 'path';
 dotenv.config();
 
 import authRouter from "./routes/auth.route.js"
@@ -17,12 +18,20 @@ app.listen(process.env.PORT,()=>{
 }).catch((error)=>{
   console.log(error)
 })
+
+const __dirname = path.resolve();
 app.use(express.json());
 
 app.use(cookieParser());
 app.use("/api/user",userRouter)
 app.use("/api/auth",authRouter)
 app.use("/api/listing",listingRouter)
+ 
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 app.use((err,req,res,next) =>{
 const statusCode = err.statusCode || 500;
