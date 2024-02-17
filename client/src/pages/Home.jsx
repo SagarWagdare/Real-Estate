64c0590f551fc524899b9e3865c8982ba8915b69
Sagar Wagdare <sagarwagdare2001@gmail.com>
2024-02-18 00:22:15 +0530

    home page navigation changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -65,7 +65,7 @@ const Home = () => {
           <br />
           Our expert support are always available.
         </div>
-        <Link to={currentUser?"/":"/sign-up"} className=" text-blue-700 font-semibold hover:underline">
+        <Link to={currentUser?"/":"/sign-in"} className=" text-blue-700 font-semibold hover:underline">
 
           Let&apos;s get started...
         </Link>
